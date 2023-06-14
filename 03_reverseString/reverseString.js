e7e6b694b514f6721f21abfe0c3b15611ed0447f
seanaelius <dc.seanaelius@gmail.com>
2023-06-14 19:07:48 -0400

    Finish up to exercise 3 Halfway through exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    //Create an array for the list of letters
+    let stringArray = string.split("");
+    //Reverse array
+    let reverseArray = stringArray.reverse();
+    //Convert to a string afterwards
+    reverse = reverseArray.join("");
+    //PROFIT
+    return reverse
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    //Store array into a variable
+    let originalArray = array;
+    //Store arguments into a variable
+    let removeItems = args;
+    //Look through each item in the original array
+    originalArray.forEach((item) => {
+        if (item == removeItems) {
+            originalArray.splice(originalArray.indexOf(item), 1)
+        }
+    });
+    return originalArray
 };
 
 // Do not edit below this line
