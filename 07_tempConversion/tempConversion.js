71b6643c734d5be88fa21cd78185312945a9cb59
seanaelius <dc.seanaelius@gmail.com>
2023-06-15 13:15:16 -0400

    Finish exercises 6 and 7 of javascript exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = (temp-32)*(5/9)
+  if (convertedTemp % 1 === 0) {
+    return convertedTemp
+  } else {
+    return Number(convertedTemp.toFixed(1))
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = (temp*(9/5)) + 32
+  if (convertedTemp % 1 === 0) {
+    return convertedTemp
+  } else {
+    return Number(convertedTemp.toFixed(1))
+  }
 };
 
 // Do not edit below this line
